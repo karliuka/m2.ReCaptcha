9e3059c29ddbe2e24b8c6fef033509b8613fe2b0
karliuka <karliuka.vitalii@gmail.com>
2018-01-18 09:11:34 +0200

    ajax plugin

---
+++ Plugin/Customer/Controller/Ajax/LoginPlugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Faonni\ReCaptcha\Plugin\Customer\Controller\Ajax;
+
+/**
+ * Ajax Login Plugin
+ */
+class LoginPlugin
+{
+}
