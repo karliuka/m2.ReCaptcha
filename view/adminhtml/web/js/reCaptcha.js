3ef90900583230ca89836bc0fec98dcc21a5ccc0
karliuka <karliuka.vitalii@gmail.com>
2018-02-27 23:27:33 +0200

    js

---
+++ view/adminhtml/web/js/reCaptcha.js
@@ -0,0 +1,53 @@
+/**
+ * Copyright © 2011-2018 Karliuka Vitalii(karliuka.vitalii@gmail.com)
+ * 
+ * See COPYING.txt for license details.
+ */ 
+define([
+    'jquery',
+	'domReady!'
+], 
+function($) {
+    'use strict';
+
+	$.widget('faonni.reCaptcha', {
+		
+        /**
+         * Widget Config Option
+         * @var {Object}
+         */		
+		options: {
+			type: 'image',
+			size: 'normal',
+			theme: 'light',
+			sitekey: null		
+		},
+		
+		/**
+		 * Initialize Widget
+		 * @returns {void}
+		 */		
+		_create: function() {
+			if ($(this.element).hasClass('g-recaptcha')) {
+				this._render(this.element.get(0));
+			} else {
+				$(this.element).find('.g-recaptcha').each(function(index, element) {
+					this._render(element);
+				}.bind(this));					
+			}
+		},
+		
+		/**
+		 * Render reCaptcha
+		 * @param {Element} element
+		 * @returns {void}
+		 */		
+		_render: function(element) {
+			if (typeof grecaptcha != 'undefined') {
+				grecaptcha.render(element, this.options);				
+			}
+		}			
+	});
+ 
+    return $.faonni.reCaptcha;
+});
