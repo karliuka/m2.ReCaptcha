71b0b58e758ac069f8daa4c67a81750888e7714f
karliuka <karliuka.vitalii@gmail.com>
2020-11-09 21:04:45 +0200

    replace client

--- Model/Provider.php
+++ Model/Provider.php
@@ -6,6 +6,10 @@
 namespace Faonni\ReCaptcha\Model;
 
 use Magento\Framework\HTTP\PhpEnvironment\RemoteAddress;
+use Psr\Log\LoggerInterface;
+use Laminas\Http\Request;
+use Laminas\Http\Response;
+use Laminas\Http\Client;
 
 /**
  * Faonni ReCaptcha Provider
@@ -17,24 +21,44 @@ class Provider
      *
      * @var string
      */
-    protected $url = 'https://www.google.com/recaptcha/api/siteverify';
+    private $url = 'https://www.google.com/recaptcha/api/siteverify';
 
     /**
      * RemoteAddress instance
      *
      * @var RemoteAddress
      */
-    protected $remoteAddress;
+    private $remoteAddress;
+
+    /**
+     * Http client
+     *
+     * @var Client
+     */
+    private $httpClient;
+
+    /**
+     * Logger
+     *
+     * @var LoggerInterface
+     */
+    private $logger;
 
     /**
      * Initialize model
      *
      * @param RemoteAddress $remoteAddress
+     * @param LoggerInterface $logger
+     * @param Client $httpClient
      */
     public function __construct(
-        RemoteAddress $remoteAddress
+        RemoteAddress $remoteAddress,
+        LoggerInterface $logger,
+        Client $httpClient
     ) {
         $this->remoteAddress = $remoteAddress;
+        $this->logger = $logger;
+        $this->httpClient = $httpClient;
     }
 
     /**
@@ -46,34 +70,58 @@ class Provider
      */
     public function validate($recaptcha, $secret)
     {
-        $client = $this->getClient($this->url);
-        $client->setParameterPost([
-            'secret'   => $secret,
-            'response' => $recaptcha,
-            'remoteip' => $this->remoteAddress->getRemoteAddress(),
-        ]);
-
-        $response = $client->request(\Zend_Http_Client::POST);
-        if ($response->isSuccessful()) {
-            $json = json_decode($response->getBody());
-            if (!empty($json->success) && true == $json->success) {
-                return true;
+        try {
+            $response = $this->getResponse($recaptcha, $secret);
+            if ($response->isSuccess()) {
+                $json = json_decode($response->getBody());
+                if (!empty($json->success) && true == $json->success) {
+                    return true;
+                }
             }
+        } catch (Exception $e) {
+            $this->logger->critical($e);
         }
         return false;
     }
 
     /**
-     * Returns the Zend Http Client
+     * Retrieve service response
      *
-     * @param string $url
-     * @return \Zend_Http_Client
+     * @param string $recaptcha
+     * @param string $secret
+     * @return Response
      */
-    public function getClient($url)
+    private function getResponse($recaptcha, $secret)
     {
-        return new \Zend_Http_Client($url, [
-            'adapter'     => 'Zend_Http_Client_Adapter_Curl',
-            'curloptions' => [CURLOPT_SSL_VERIFYPEER => false],
-        ]);
+        return $this->getClient()->setUri($this->url)
+            ->setMethod(Request::METHOD_POST)
+            ->setParameterPost($this->getParameterPost($recaptcha, $secret))
+            ->send();
+    }
+
+    /**
+     * Retrieve post parameters
+     *
+     * @param string $recaptcha
+     * @param string $secret
+     * @return mixed[]
+     */
+    private function getParameterPost($recaptcha, $secret)
+    {
+        return [
+            'secret'   => $secret,
+            'response' => $recaptcha,
+            'remoteip' => $this->remoteAddress->getRemoteAddress(),
+        ];
+    }
+
+    /**
+     * Retrieve http client
+     *
+     * @return Client
+     */
+    private function getClient()
+    {
+        return $this->httpClient;
     }
 }
